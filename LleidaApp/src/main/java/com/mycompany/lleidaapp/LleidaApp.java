6b5a66d917c01c8cf6195f911e0dcd7ebd79ff92
Andres <andres.jaag@hotmail.es>
2022-06-22 15:17:45 -0500

    start

---
+++ LleidaApp/src/main/java/com/mycompany/lleidaapp/LleidaApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lleidaapp;
+
+import INTERFAZ.JFLogin;
+
+/**
+ *
+ * @author Jess
+ */
+public class LleidaApp {
+
+    public static void main(String[] args) {
+        JFLogin jfl=new JFLogin();
+        jfl.setVisible(true);
+    }
+}
